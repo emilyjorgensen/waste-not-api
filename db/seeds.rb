7fd0b245f7ca07421088571b40711aaf9db6e270
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-20 15:34:21 -0600

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Ingredient.create(name: "Eggs", image_url: "https://images.theconversation.com/files/372017/original/file-20201130-21-zuzh18.jpg?ixlib=rb-1.1.0&rect=44%2C8%2C5852%2C3925&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip")
+Ingredient.create(name: "Bell Pepper", image_url: "https://media.istockphoto.com/id/153564958/photo/colorful-peppers.jpg?s=612x612&w=0&k=20&c=7Ce6whyOFcnTQi74emPSwAdaXEcdMQI1JVNjfeTRP-Y=")
+Ingredient.create(name: "Lentils", image_url: "https://www.shutterstock.com/image-photo/lentils-spoon-wooden-bowl-close-260nw-522209995.jpg")
+Ingredient.create(name: "Feta Cheese", image_url: "https://media.istockphoto.com/id/608615220/photo/greek-feta-cheese-cubes-in-wooden-bowl-over-white.jpg?s=612x612&w=0&k=20&c=b0N3_OGu3wgSfT-hNQ-j-k642_OU2JF40O2YGpWuCvE=")
+Ingredient.create(name: "Steak", image_url: "https://media.istockphoto.com/id/505207430/photo/fresh-raw-beef-steak.jpg?s=612x612&w=0&k=20&c=QxOege3Io4h1TNJLtGYh71rxb29p1BfFcZvCipz4WVY=")
+Ingredient.create(name: "Wheat Bread", image_url: "https://media.istockphoto.com/id/157587362/photo/detailed-close-up-of-sliced-grain-bread-on-white-background.jpg?s=612x612&w=0&k=20&c=nr5f0Mb3Dx9RP_LmKKwlo6IlzDebfIdCbnERsnoSG94=")
+Ingredient.create(name: "Pepperoni", image_url: "https://t4.ftcdn.net/jpg/01/84/28/67/360_F_184286756_q9Y3oFBVBotnWvtCxuVGE7XlaVuBHFAx.jpg")
+Ingredient.create(name: "Mushrooms", image_url: "https://media.istockphoto.com/id/1276597176/photo/button-mushrooms.jpg?s=612x612&w=0&k=20&c=ilDpArGu1tDBzSAZR8zxr5VXjet7tb7U7Erx-vsRY4E=")
+Ingredient.create(name: "Italian Dressing", image_url: "https://media.istockphoto.com/id/182818621/photo/salad-dressing.jpg?s=612x612&w=0&k=20&c=W2yhT9nFCWriibgRIPNcxw_FgHiFS9DG5XuW9K5AJvk=")
+Ingredient.create(name: "Salt", image_url: "https://media.istockphoto.com/id/516450576/photo/traditional-glass-salt-cellar.jpg?s=612x612&w=0&k=20&c=H5gLaxswJnnEKRXDbusy-uRCiARroiM-lWRZHjVgV3Y=")
