f3801982d12030b5ae7619b139684cd391dd0669
emjorg10 <144714071+emjorg10@users.noreply.github.com>
2023-12-21 14:27:12 -0600

    pantry_item create action

--- app/controllers/pantry_items_controller.rb
+++ app/controllers/pantry_items_controller.rb
@@ -3,4 +3,15 @@ class PantryItemsController < ApplicationController
     @pantry_items = PantryItem.all
     render :index
   end
+
+  def create
+    @pantry_item = PantryItem.create(
+      user_id: params[:user_id],
+      ingredient_id: params[:ingredient_id],
+      amount: params[:amount],
+      category: params[:category],
+      use_by_date: params[:use_by_date],
+    )
+    render :show
+  end
 end
--- test/controllers/pantry_items_controller_test.rb
+++ test/controllers/pantry_items_controller_test.rb
@@ -8,4 +8,11 @@ class PantryItemsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal PantryItem.count, data.length
   end
+
+  test "create" do
+    assert_difference "PantryItem.count", 1 do
+      post "/pantry_items.json", params: { user_id: 1, pantry_item: 1, amount: "1 block", category: "pantry", use_by_date: "01/01/25" }
+      assert_response 200
+    end
+  end
 end
